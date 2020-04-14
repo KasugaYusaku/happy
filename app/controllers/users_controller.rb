fecdaf59f9859b8738edd38ffdbdd6e727fecc88
KasugaYusaku <ec2-user@ip-172-31-39-232.ap-northeast-1.compute.internal>
2020-04-14 08:23:02 +0000

    secondcommit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path success: '登録完了しました'
     else
+      flash.now[:danger] = "登録失敗しました"
       render :new
     end
   end
   
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
